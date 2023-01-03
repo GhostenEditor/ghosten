6ca0c0e3edb65ac5d2ba55aaf780d94f17f7f12c
yangg <ghosteneditor@users.noreply.github.com>
2023-01-04 00:46:30 +0800

    build: 调整tauri相关的构建设置

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,26 +1,10 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
 )]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-// use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
-
 fn main() {
-    // let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    // let close = CustomMenuItem::new("close".to_string(), "Close");
-    // let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
-    // let menu = Menu::new()
-    //     .add_native_item(MenuItem::Copy)
-    //     .add_item(CustomMenuItem::new("hide", "Hide"))
-    //     .add_submenu(submenu);
-    tauri::Builder::default()
-        // .menu(menu)
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
