14a56cc2245b2fae9d7b795736a9c1efb06dcc13
yangg <ghosteneditor@users.noreply.github.com>
2023-02-02 01:26:50 +0800

    fix: 大量bug修复

--- projects/editor/src/lib/classes/gt-event.ts
+++ projects/editor/src/lib/classes/gt-event.ts
@@ -18,6 +18,12 @@ export class GtEventTarget extends EventTarget {
   ) {
     super.addEventListener(type, callback, options);
   }
+
+  // @ts-ignore
+  override removeAllListeners() {
+    // @ts-ignore
+    super.removeAllListeners!();
+  }
 }
 
 export class ChangeActionStartEvent extends Event {
